574021aa2e0cd3d4bf2896e74b7c42c3c71db675
ibrahimalmasri <ibrahimalmasri268@gmail.com>
2024-08-09 15:43:40 +0300

    first summer_delivery

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import React from 'react';
+import { Login ,MainScreen ,InProgressScreen ,StoreArrivalScreen ,DeliveryDetailsScreen ,EditProfileScreen ,Footer } from './screens';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Welcome'
+      >
+     
+      <Stack.Screen 
+        name="Login"
+        component={Login} 
+        options={{
+          headerShown: false
+        }}
+        />
+
+      <Stack.Screen 
+        name="MainScreen"
+        component={MainScreen} 
+        options={{
+          headerShown: false
+        }}
+        />
+           <Stack.Screen 
+        name="InProgressScreen"
+        component={InProgressScreen} 
+        options={{
+          headerShown: false
+        }}
+        />
+
+          <Stack.Screen 
+            name="StoreArrivalScreen"
+            component={StoreArrivalScreen} 
+            options={{
+              headerShown: false
+            }}
+            />
+
+
+          <Stack.Screen 
+            name="DeliveryDetailsScreen"
+            component={DeliveryDetailsScreen} 
+            options={{
+              headerShown: false
+            }}
+            />
+
+          <Stack.Screen 
+            name="EditProfileScreen"
+            component={EditProfileScreen} 
+            options={{
+              headerShown: false
+            }}
+            />
+            <Stack.Screen 
+            name="Footer"
+            component={Footer} 
+            options={{
+              headerShown: false
+            }}
+            />
+    
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
